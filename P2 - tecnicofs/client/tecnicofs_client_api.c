3462448d26e692a746f6c86b2afdbd1d384e0ad7
Vasco Afonso <vafonso2002@gmail.com>
2022-01-31 18:49:38 +0000

    minimal changes client

--- P2 - tecnicofs/client/tecnicofs_client_api.c
+++ P2 - tecnicofs/client/tecnicofs_client_api.c
@@ -1,8 +1,55 @@
 #include "tecnicofs_client_api.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 40
+
+
+int session_id;
+
+void send_code(int tx, char const *str){
+    
+    ssize_t ret = write(tx, str, BUFFER_SIZE);
+
+}
+
+
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     /* TODO: Implement this */
-    return -1;
+    char buffer[BUFFER_SIZE];
+
+    if (unlink(server_pipe_path) != 0 && errno != ENOENT) {
+        return -1;
+    }
+
+    // create pipe
+    if (mkfifo(server_pipe_path, 0640) != 0) {
+        return -1;
+    }
+
+    // open pipe for writing
+    // this waits for someone to open it for reading
+    int tx = open(server_pipe_path, O_WRONLY);
+    if (tx == -1) {
+        return -1;
+    }
+
+    int rx = open(client_pipe_path, O_RDONLY);
+
+    ssize_t ret = read(rx, buffer, BUFFER_SIZE - 1);
+
+    
+
+
+
+
+    return 0;
 }
 
 int tfs_unmount() {
--- P2 - tecnicofs/fs/tfs_server.c
+++ P2 - tecnicofs/fs/tfs_server.c
@@ -1,4 +1,11 @@
 #include "operations.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 
@@ -11,6 +18,10 @@ int main(int argc, char **argv) {
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
     /* TO DO */
+    int rx = open(pipename, O_RDONLY);
+
+    
+
 
     return 0;
 }
